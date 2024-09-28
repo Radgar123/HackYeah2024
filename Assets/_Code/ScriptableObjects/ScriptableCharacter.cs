770c125b8efff0d8b042ffe0b4bdcf20e1a4f418
Radgar123 <jakubbera21@gmail.com>
2024-09-28 15:00:28 +0200

    Scriptable Characters

---
+++ Assets/_Code/ScriptableObjects/ScriptableCharacter.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "CharacterData", menuName = "ScriptableObjects/Character/characterData", order = 1)]
+public class ScriptableCharacter : ScriptableObject
+{
+    [Header("Character Params")]
+    public CharacterType characterType;
+    
+    public string characterName;
+    
+    public int energy;
+    public int attack;
+    
+    public float attackSpeed;
+    public float dodgeRatio;
+    
+    [Header("Shops Params")]
+    public int characterCost;
+    public int characterSell;
+}
+
+public enum CharacterType
+{
+    Player,
+    Enemy
+}
