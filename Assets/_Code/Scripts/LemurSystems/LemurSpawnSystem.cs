b8f7a7a8bf52bd87b6b2f491caa7ac1bb3eb861b
Damian Buczek <88405026+SZtamjan@users.noreply.github.com>
2024-09-28 16:29:43 +0200

    Spawn for enemy and player characters

--- Assets/_Code/Scripts/LemurSystems/LemurSpawnSystem.cs
+++ Assets/_Code/Scripts/LemurSystems/LemurSpawnSystem.cs
@@ -1,39 +1,70 @@
 ﻿using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace _Code.Scripts.LemurSystems
 {
     public class LemurSpawnSystem : MonoBehaviour
     {
         [SerializeField] private GameObject lemurObject;
-        [SerializeField] private List<Transform> spawnPoints;
+        [SerializeField] private List<Transform> playerSpawnPoints;
+        [SerializeField] private List<Transform> enemySpawnPoints;
 
         #region Debug
 
         [Header("Debug")]
-        [SerializeField] private List<ScriptableCharacter> debugCharacters;
+        [SerializeField] private List<GameObject> debugCharacters;
+        [SerializeField] private List<GameObject> debugEnemyCharacters;
         
         [Button]
         public void DebugSpawner()
         {
-            SpawnLemurs(debugCharacters);
+            LoadNewFight(debugCharacters,debugEnemyCharacters);
         }
 
         #endregion
 
-        public void SpawnLemurs(List<ScriptableCharacter> scriptableCharacters)
+        public void LoadNewFight(List<GameObject> playerCharacters, List<GameObject> enemyCharacters)
         {
-            if (scriptableCharacters.Count > 5)
+            InitSpawnLemurs(playerCharacters);
+            InitSpawnLemurs(enemyCharacters);
+        }
+        
+        private void InitSpawnLemurs(List<GameObject> characters)
+        {
+            if (characters.Count <= 0)
             {
-                Debug.LogError($"Over 5 lemurs to spawn, quiting");
+                Debug.LogWarning("0 or less lemurs to spawn");
+                return;
+            }
+            
+            switch (characters[0].GetComponent<LemurManager>().scriptableCharacter.characterType)
+            {
+                case CharacterType.Player:
+                    SpawnLemurs(characters,playerSpawnPoints);
+                    break;
+                case CharacterType.Enemy:
+                    SpawnLemurs(characters,enemySpawnPoints);
+                    break;
+                default:
+                    Debug.LogError("No character type, or unsupported");
+                    break;
+            }
+        }
+
+        private void SpawnLemurs(List<GameObject> characters,List<Transform> currSpawnPoints)
+        {
+            if (characters.Count > currSpawnPoints.Count)
+            {
+                Debug.LogError($"Over {currSpawnPoints.Count} lemurs to spawn, quiting - value driven by spawnPoints");
                 return;
             }
     
-            for (int i = 0; i < scriptableCharacters.Count; i++)
+            for (int i = 0; i < characters.Count; i++)
             {
-                GameObject currentLemur = Instantiate(lemurObject,spawnPoints[i]);
-                currentLemur.GetComponent<LemurManager>().scriptableCharacter = scriptableCharacters[i];
+                GameObject currentLemur = Instantiate(lemurObject,currSpawnPoints[i]);
+                //currentLemur.GetComponent<LemurManager>().scriptableCharacter = scriptableCharacters[i];
             }
         }
     }
