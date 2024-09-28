7845a49b9020e6e17000bbbdfcea2474ace442a6
Damian Buczek <88405026+SZtamjan@users.noreply.github.com>
2024-09-28 15:17:29 +0200

    Base

---
+++ Assets/_Code/Scripts/LemurSpawnSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Code.Scripts
+{
+    public class LemurSpawnSystem : MonoBehaviour
+    {
+        public void SpawnLemurs(List<ScriptableCharacter> scriptableCharacters)
+        {
+            foreach (ScriptableCharacter character in scriptableCharacters)
+            {
+                
+            }
+        }
+    }
+}
