7429ea4909413eb8d8600058107984168550b4ea
Damian Buczek <88405026+SZtamjan@users.noreply.github.com>
2024-09-28 17:49:26 +0200

    LemurManager change

--- Assets/_Code/Scripts/LemurSystems/LemurManager.cs
+++ Assets/_Code/Scripts/LemurSystems/LemurManager.cs
@@ -1,12 +1,22 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace _Code.Scripts.LemurSystems
 {
     public class LemurManager : MonoBehaviour
     {
         [HideInInspector] public ScriptableCharacter scriptableCharacter;
-        
-        public void LemurDie()
+
+        public int DealDamage
+        {
+            set
+            {
+                scriptableCharacter.energy -= value;
+                if (scriptableCharacter.energy <= 0) KillMe();
+            }
+        }
+
+        private void KillMe()
         {
             Destroy(gameObject);
         }
