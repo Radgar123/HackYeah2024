b944321b6cd336e8e2cb447025ed8fb7e111bb25
Radgar123 <jakubbera21@gmail.com>
2024-09-29 07:57:23 +0200

    Changes On Level and Change Load Scene

--- Assets/_Code/Scripts/SceneLoader/LoadScene.cs
+++ Assets/_Code/Scripts/SceneLoader/LoadScene.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using _Code.Scripts.SceneTransitionScripts;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,8 +7,17 @@ namespace _Code.Scripts.SceneLoader
 {
     public class LoadScene : MonoBehaviour
     {
+        [SerializeField] private UIAnimations _uiControllerAnimations;
+        
         public void LoadSceneById(int id)
         {
+            _uiControllerAnimations.UITransitionFadeIn();
+            StartCoroutine(DelayedLoadScene(id));
+        }
+
+        IEnumerator DelayedLoadScene(int id)
+        {
+            yield return new WaitForSeconds(2.1f);
             SceneManager.LoadScene(id);
         }
     }
