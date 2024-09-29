f19568280652d38978733f4d5d143168608cd0b1
Damian Buczek <88405026+SZtamjan@users.noreply.github.com>
2024-09-29 11:37:32 +0200

    ui

--- Assets/_Code/Scripts/LemurSystems/FightUI/FightUIAnimations.cs
+++ Assets/_Code/Scripts/LemurSystems/FightUI/FightUIAnimations.cs
@@ -1,12 +1,13 @@
 ﻿using DG.Tweening;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace _Code.Scripts.LemurSystems.FightUI
 {
     public class FightUIAnimations : MonoBehaviour
     {
-        [SerializeField] private TextMeshProUGUI obj;
+        [SerializeField] private Image image;
         [SerializeField] private float duration = 2f;
         [SerializeField] private Transform startPos;
         public Sequence sequence;
@@ -14,18 +15,24 @@ namespace _Code.Scripts.LemurSystems.FightUI
         public void SetupAndStartAttackAnim(Vector3 endPos, string damage)
         {
             sequence = DOTween.Sequence();
-            TextMeshProUGUI newObj = Instantiate(obj.gameObject, obj.gameObject.transform.parent).GetComponent<TextMeshProUGUI>();
+            
+            Image newImage = Instantiate(image.gameObject, image.gameObject.transform.parent).GetComponent<Image>();
+            TextMeshProUGUI newObj = newImage.transform.GetComponentInChildren<TextMeshProUGUI>();
             newObj.text = damage;
             newObj.gameObject.SetActive(true);
+            newImage.gameObject.SetActive(true);
             endPos = new Vector3(endPos.x, endPos.y, endPos.z - 1f);
             Tween tw1 = newObj.gameObject.transform.DOMove(endPos, duration);
+            Tween tw2 = newImage.gameObject.transform.DOMove(endPos, duration);
 
             sequence.Append(tw1);
+            sequence.Join(tw2);
             
             sequence.OnComplete(() =>
             {
                 //Destroy(newObj.gameObject);
                 newObj.gameObject.SetActive(false);
+                image.gameObject.SetActive(false);
             });
             
             sequence.Play();
